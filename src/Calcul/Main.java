8ef099680cc21cd97c42208dc4367540e3bf7af4
18130165 <62516617+18130165@users.noreply.github.com>
2020-05-21 18:58:39 +0500

    Delete Main.java

--- src/Calcul/Main.java
+++
@@ -1,8 +0,0 @@
-package Calcul;
-public class Main {
-	public static void main(String[] args) {
-		CalculGUI start = new CalculGUI(); //СОЗДАНИЕ ОБЪЕКТА КЛАССА CalculGUI
-		start.setVisible(true);
-	}
-}
-
